f8e8f02faa55b02c6f5b4ae3e50b68f8cfb80418
ST10486285 <ST10486285@rcconnect.edu.za>
2025-09-19 18:57:28 +0000

    Initial commit

---
+++ src/main/java/com/mycompany/prog5121/PROG5121.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog5121;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class PROG5121 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
